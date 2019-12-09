0f6d8b6eb7d1b52dac105825aed468619e815853
Cellor425 <45540592+Cellor425@users.noreply.github.com>
2019-12-08 18:10:59 -0600

    Started main page layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,31 @@
 <template>
 	<div class="home">
-		<h1>Welcome</h1>
-		<p>This is the home page for my webprofile.</p>
+		<b-jumbotron bg-variant="info" text-variant="white">
+			<template v-slot:header>Welcome</template>
+
+			<template
+				v-slot:lead
+			>To the landing page for software developer and data specialist Michael Schneider.</template>
+
+			<hr class="my-4" />
+
+			<div id="content">
+				<b-tabs content-class="mt-3" align="center" fill>
+					<b-tab title="Contact Info" active>
+						<p>Contact Info</p>
+					</b-tab>
+					<b-tab title="Skills">
+						<p>Skills</p>
+					</b-tab>
+					<b-tab title="Job History">
+						<p>Job History</p>
+					</b-tab>
+					<b-tab title="Awards">
+						<p>Awards</p>
+					</b-tab>
+				</b-tabs>
+			</div>
+		</b-jumbotron>
 	</div>
 </template>
 
@@ -11,5 +35,20 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+#content {
+	background-color: mintcream;
+}
+
+ul.nav-tabs {
+	background-color: mediumseagreen;
+}
+
+a {
+	color: white;
+}
+
+.tab-pane {
+	color: black;
+}
 </style>
