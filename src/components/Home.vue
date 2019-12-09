06140a60d7166d68d2757aaa6d99f78c95790c99
Cellor425 <45540592+Cellor425@users.noreply.github.com>
2019-12-08 23:32:20 -0600

    Added Resume and Cover Letter to home tabs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,23 +5,23 @@
 
 			<template
 				v-slot:lead
-			>To the landing page for software developer and data specialist Michael Schneider.</template>
+			>To the landing page for software developer and database specialist Michael Schneider.</template>
 
 			<hr class="my-4" />
 
 			<div id="content">
 				<b-tabs content-class="mt-3" align="center" fill>
-					<b-tab title="Contact Info" active>
-						<p>Contact Info</p>
+					<b-tab no-body title="Contact Info" active>
+						<p>Contact Info email, phone, address, linkedin, github</p>
 					</b-tab>
-					<b-tab title="Skills">
-						<p>Skills</p>
+					<b-tab title="About">
+						<p>List about me Name, Occupation, Image, Description</p>
 					</b-tab>
-					<b-tab title="Job History">
-						<p>Job History</p>
+					<b-tab no-body title="Resume">
+						<b-card-img bottom :src="require('../assets/Resume.png')"></b-card-img>
 					</b-tab>
-					<b-tab title="Awards">
-						<p>Awards</p>
+					<b-tab no-body title="Cover Letter">
+						<b-card-img bottom :src="require('../assets/CoverLetter.png')"></b-card-img>
 					</b-tab>
 				</b-tabs>
 			</div>
@@ -31,7 +31,11 @@
 
 <script>
 export default {
-	name: "Home"
+	name: "Home",
+	components: {},
+	data() {
+		return {};
+	}
 };
 </script>
 
@@ -44,7 +48,7 @@ ul.nav-tabs {
 	background-color: mediumseagreen;
 }
 
-a {
+a.nav-link {
 	color: white;
 }
 
