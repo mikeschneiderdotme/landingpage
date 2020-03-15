b604ab758144321646da7fce34fc03c2639ffef6
Cellor425 <45540592+Cellor425@users.noreply.github.com>
2020-03-15 17:06:20 -0500

    Added awx plugins

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
+
+Vue.use(BootstrapVue, AmplifyPlugin, AmplifyModules)
 
 
 Vue.config.productionTip = false;
